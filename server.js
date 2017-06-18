b25dd6d33ad410f78d15893921e15210c946a1b2
Mark Eaton <mark.eaton@vroom.com>
2017-06-18 17:36:34 -0500

    ?

--- server.js
+++ server.js
@@ -16,16 +16,47 @@ pwm = new Pca9685Driver(options, function(err) {
         process.exit(-1);
     }
     console.log("Initialization done");
+
+    // Set channel 0 to turn on on step 42 and off on step 255
+    // (with optional callback)
+    pwm.setPulseRange(0, 42, 255, function() {
+        if (err) {
+            console.error("Error setting pulse range.");
+        } else {
+            console.log("Pulse range set.");
+        }
+    });
+
+    // Set the pulse length to 1500 microseconds for channel 2
+    pwm.setPulseLength(2, 1500);
+
+    // Set the duty cycle to 25% for channel 8
+    pwm.setDutyCycle(8, 0.25);
+
+    // Turn off all power to channel 6
+    // (with optional callback)
+    pwm.channelOff(0, function() {
+        if (err) {
+            console.error("Error turning off channel.");
+        } else {
+            console.log("Channel 0 is off.");
+        }
+    });
+
+    pwm.channelOff(3, function() {
+        if (err) {
+            console.error("Error turning off channel.");
+        } else {
+            console.log("Channel 3 is off.");
+        }
+    });
+
+    // Turn on channel 3 (100% power)
+    //pwm.channelOn(3);
 });
 
-app.get('/left', function (req, res) {
-  res.send('left')
-  pwm.setPulseLength(0, 600);
-})
-
-app.get('/right', function (req, res) {
-  res.send('right')
-  pwm.setPulseLength(0, 250);
+app.get('/', function (req, res) {
+  res.send('Hello World!')
 })
 
 app.listen(3000, function () {
