97e028ae3e646361273f4e1ed127b2f4a5c7135f
Mark Eaton <mark.eaton@vroom.com>
2017-06-18 16:26:09 -0500

    added adafruit driver package

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const makePwmDriver = require('adafruit-i2c-pwm-driver')
+const pwmDriver = makePwmDriver({address: 0x40, device: '/dev/i2c-1'})
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
+  pwmDriver.setPWMFreq(50)
+  pwmDriver.setPWM(2) // channel, on , off 
 })
 
 app.listen(3000, function () {
