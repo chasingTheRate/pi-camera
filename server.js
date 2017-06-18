4fb83e1dfac41c4fa270101864b3f0eeb6f1d2b4
Mark Eaton <mark.eaton@vroom.com>
2017-06-18 16:37:07 -0500

    const happy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const i2cBus = require("i2c-bus");
-const Pca9685Driver = require("pca9685").Pca9685Driver;
+var i2cBus = require("i2c-bus");
+var Pca9685Driver = require("pca9685").Pca9685Driver;
 
 var options = {
     i2c: i2cBus.openSync(1),
@@ -47,7 +47,7 @@ app.get('/', function (req, res) {
 
     // Turn on channel 3 (100% power)
     pwm.channelOn(3);
-}); 
+});
 })
 
 app.listen(3000, function () {
