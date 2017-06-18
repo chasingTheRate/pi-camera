ab8f3934db309a6fe80bc7814ed5a68d6f27a7f6
Mark Eaton <mark.eaton@vroom.com>
2017-06-18 17:13:14 -0500

    added initial options

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const app = express();
 var i2c = require('i2c-bus')
 var Pca9685Driver = require("pca9685").Pca9685Driver;
 
+var options = {
+    i2c: i2cBus.openSync(1),
+    address: 0x40,
+    frequency: 50,
+    debug: false
+};
+
 pwm = new Pca9685Driver(options, function(err) {
     if (err) {
         console.error("Error initializing PCA9685");
